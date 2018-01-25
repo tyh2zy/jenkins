b5268cbb53aa10a9e4a6af9a99783da021efddc3
Gogs <gogs@fake.local>
2018-01-25 14:13:32 +0800

    two

---
+++ web_3/src/web_3/LoginServlet.java
@@ -0,0 +1,60 @@
+package web_3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		System.out.println("服务器收到客户端请求："+userName+":"+password);
+		PrintWriter out = response.getWriter();
+		
+		out.write("服务器发送信息给客户端："+userName+":"+password);
+		out.println("");
+		out.flush();
+		out.close();
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
